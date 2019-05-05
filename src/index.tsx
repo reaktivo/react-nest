b42aad7ee620a64a563df70b2a184ff50960d21f
Marcel Miranda <m@reaktivo.com>
2019-05-05 17:27:22 +0200

    Use React.isValidElement to check for React element

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,10 @@
-import React, { Children, cloneElement, Fragment, ReactNode } from 'react';
+import React, {
+  Children,
+  cloneElement,
+  Fragment,
+  isValidElement,
+  ReactNode,
+} from 'react';
 
 type Props = {
   children?: ReactNode;
@@ -9,14 +15,13 @@ export default function Nest(props: Props) {
     <Fragment>
       {Children.toArray(props.children)
         .reverse()
-        .reduce((child, parent) => {
-          const el = parent as React.ReactElement;
-          if (!el || !el.type) {
-            return child;
-          }
-          const { children, ...props } = el.props;
-          return cloneElement(el, props, [children, child]);
-        }, null)}
+        .reduce(
+          (child, parent) =>
+            isValidElement(parent)
+              ? cloneElement(parent, parent.props, child)
+              : child,
+          []
+        )}
     </Fragment>
   );
 }
