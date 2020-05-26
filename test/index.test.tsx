65cbdd4470280060edb71e8698a0cbdf375a2243
Marcel Miranda <m@reaktivo.com>
2020-05-26 11:15:45 +0200

    Fixed deps

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext } from 'react';
-import { render } from 'react-testing-library';
+import { render } from '@testing-library/react';
 import Nest from '../src';
 
 describe('Nest', () => {
